0b669e483f3ad1d3b8cca0401472cb62e10cb453
BoBoBo <BoBoBo@BoBoBos-MacBook-Air.local>
2019-10-20 13:18:43 +0800

    add install.sh

---
+++ configuration/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/Kashomon/vimporter.git ~/.vim/plugin/vimporter
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"

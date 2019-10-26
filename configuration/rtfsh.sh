cae4855ff8575c692aac88a06ac3d21e236ecd2b
James <james_email@sina.cn>
2019-10-26 22:14:54 +0800

    Save daily.

--- configuration/rtfsh.sh
+++ configuration/rtfsh.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+dir_rtfsc=~/code/rtfsc
 
-git clone https://github.com/spring-projects/spring-data-redis.git ~/code/rtfsc/spring-data-redis
+gitclone(){
+    if [ -d $2 ]; then
+        echo $2 exists.
+    else
+        git clone $1 $2
+    fi
+}
 
-git clone https://github.com/yomonah/vue-demo.git ~/code/rtfsc/example/vue-demo
-git clone https://github.com/AndreiD/Flask-Easy-Template.git ~/code/rtfsc/example/Flask-Easy-Template
+
+gitclone https://github.com/yomonah/vue-demo.git $dir_rtfsc/example/vue-demo
+gitclone https://github.com/AndreiD/Flask-Easy-Template.git $dir_rtfsc/example/Flask-Easy-Template
+
+gitclone https://github.com/spring-projects/spring-data-redis.git $dir_rtfsc/spring-data-redis
+gitclone https://github.com/netty/netty.git $dir_rtfsc/netty
+gitclone https://github.com/scrapy/scrapy.git $dir_rtfsc/scrapy

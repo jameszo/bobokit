7d52d005f4ad2e2f92407e124b9414b4f7c35de0
BoBoBo <BoBoBo@BoBoBos-MacBook-Air.local>
2019-10-26 20:53:52 +0800

    Save daily.

---
+++ daily/commit-code.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+gitpull(){
+    git add .
+    git commit -m "Save daily." .
+    git pull origin master
+}
+
+cd ~/code
+for d in `ls`
+do
+    if [ -d $d/.git ]; then
+        cd $d
+        echo $d
+        gitpull
+        cd ..
+    fi
+done

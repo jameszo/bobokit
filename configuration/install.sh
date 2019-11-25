42f9136c09e543f8e84c1bf02a23abbb15fb5326
BoBoBo <bobobo@BoBoBos-MacBook-Pro.local>
2019-11-25 09:13:10 +0800

    Save daily.

--- configuration/install.sh
+++ configuration/install.sh
@@ -2,11 +2,27 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Kashomon/vimporter.git ~/.vim/plugin/vimporter
-git clone git@github.com:jameszo/markdown-css.git ~
+git clone git@github.com:jameszo/markdown-css.git ~/markdown-css
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#For docker in macos
 brew cask install osxfuse
 brew install sshfs
+
+brew install docker
+brew install docker-machine
+brew install docker-swarm
+brew install docker-machine-driver-xhyve
+
+brew install autojump
+brew cask install dash
+brew cask install iterm2
+
+brew install openjdk@12
+sudo ln -sfn /usr/local/opt/openjdk@12/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-12.jdk
+
+brew install plantuml
+brew install pandoc
+brew install nmap
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

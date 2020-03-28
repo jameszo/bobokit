f7859324996437c3903b3aabdb083c7be98b2d6b
BoBoBo <bobobo@BoBoBos-MacBook-Air.local>
2020-03-28 20:20:57 +0800

    Save daily.

--- subject/file/base_text.py
+++ subject/file/base_text.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# author:   James Zo
-# email:    james_email@sina.cn
+# author:   BoBoBo
+# email:    bobobonet@hotmail.com 
 
 import os
 import codecs
@@ -14,37 +14,38 @@ if sys.getdefaultencoding() != 'utf-8':
 def traverse_path_line(path, deal):
     file_list = os.listdir(path)
     for f in file_list:
-        filepath = os.path.join(path, f)
-        if os.path.isdir(filepath):
-            self._traverse_path_count(filepath)
+        file_path = os.path.join(path, f)
+        if os.path.isdir(file_path):
+            self.traverse_path_count(file_path)
         else:
-            with open(filepath) as fo:
+            with open(file_path) as fo:
                 for line in fo:
                     deal(line)
 
 def traverse_path_file(path, deal):
     file_list = os.listdir(path)
     for f in file_list:
-        filepath = os.path.join(path, f)
-        if os.path.isdir(filepath):
-            self._traverse_path_count(filepath)
+        file_path = os.path.join(path, f)
+        if os.path.isdir(file_path):
+            self.traverse_path_count(file_path)
         else:
-            with open(filepath) as fo:
+            with open(file_path) as fo:
                 deal(fo.read())
 
-def copyForWindows(self):
-    copy_file = open(self.path + os.path.sep + self.shortname + "_win"  + self.extension, 'w')
-    index_src = self.fo.tell()
-    self.fo.seek(0)
-    line = self.fo.readline()
+def make_windows_copy(file_path, copy_path):
+    fo = open(file_path, 'r')
+    copy_file = open(copy_path, 'w')
+    index_src = fo.tell()
+    fo.seek(0)
+    line = fo.readline()
     while line:
         copy_file.write(codecs.BOM_UTF8)
         copy_file.write(line.encode('utf-8'))
         copy_file.flush()
-        line = self.fo.readline()
-    self.fo.seek(index_src)
+        line = fo.readline()
+    fo.close()
 
-class BaseFile:
+class BaseTextFile:
     def __init__(self, file_path, access_mode='a'):
         self.fo = open(file_path, access_mode)
         (path, file_name) = os.path.split(file_path);  
@@ -55,19 +56,16 @@ class BaseFile:
         self.shortname = shortname
         self.extension = extension
 
-    def write_line(self, line):
+    def write_by_line(self, line, flush_now):
         line = line + "\r\n"
         self.fo.write(line)
-        self.fo.flush()
+        if flush_now:
+            self.fo.flush()
 
-    def getAllContent(self):
-        str = '';
-        contents = self.fo.readlines()
-        for line in contents:
-            str = str + "\r\n" + line
-        return str
+    def flush_file(self):
+        self.fo.flush()
 
     def close(self):
-        self.fo.flush()
+        flush_file()
         self.fo.close()
 

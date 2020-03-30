0bf6d3791ca9d35cce76b500d3e1b672e2bdcfa8
BoBoBo <bobobo@BoBoBos-MacBook-Pro.local>
2020-03-30 10:56:43 +0800

    Save daily.

--- hand/tests/test_file.py
+++ hand/tests/test_file.py
@@ -7,12 +7,20 @@
 
 import unittest
 import os
+import shutil
+
+TEST_DIR = "./test_tmp"
 
 class TestFile(unittest.TestCase):
 
+
     def setUp(self):
-        if os.path.exists("./.test_tmp"):
-            os.makedirs("./
+        if os.path.exists(TEST_DIR):
+            shutil.rmtree(TEST_DIR)
+        os.makedirs(TEST_DIR + "/d1/d11/d111");
+        os.makedirs(TEST_DIR + "/d2/d21");
+        with open(TEST_DIR + "/f1", 'w') as f1:
+            f1.writelines(["f1:1","f1:2"])
 
     def test_traverse_file(self):
         self.fail("TODO")

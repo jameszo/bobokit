d355e97cdb828e92bf46286b3c345e75595ffffc
BoBoBo <bobobo@BoBoBos-MacBook-Air.local>
2020-03-30 09:22:55 +0800

    Save daily.

---
+++ hand/tests/test_file.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+"""
+    author:   BoBoBo
+    email:    bobobonet@hotmail.com 
+"""
+
+import unittest
+import os
+
+class TestFile(unittest.TestCase):
+
+    def setUp(self):
+        if os.path.exists("./.test_tmp"):
+            os.makedirs("./
+
+    def test_traverse_file(self):
+        self.fail("TODO")
+
